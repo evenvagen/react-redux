f2f90968aac081ab926b6ea8cb8f460fddd1efd7
Even Vågen <evenvagen@gmail.com>
2023-02-01 10:19:03 +0100

    complete tutorial

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+import { Provider } from 'react-redux';
 import Home from './src/screens/Home';
-
+import { store } from './src/redux/store';
+ 
 export default function App() {
-  return <Home />;
+  return (
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
 }
